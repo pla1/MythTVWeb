00b0e4e62e523983f1dc783263d5b81801fe85cf
Patrick L Archibald <patrick.archibald@gmail.com>
2014-07-27 18:00:24 -0400

    Added recordings table.

---
+++ js/controllers.js
@@ -0,0 +1,58 @@
+var mythTVWebApp = angular.module('mythTVWebModule', []);
+var BACKEND="server0.pla.lcl";
+var PORT=6544;
+var RECORDINGS_PATH="/Dvr/GetRecordedList";
+
+mythTVWebApp.config(function($httpProvider) {
+
+	$httpProvider.interceptors.push(function($q, $rootScope) {
+		return {
+			'request' : function(config) {
+				$rootScope.$broadcast('loading-started');
+				return config || $q.when(config);
+			},
+			'response' : function(response) {
+				$rootScope.$broadcast('loading-complete');
+				return response || $q.when(response);
+			}
+		};
+	});
+
+});
+
+mythTVWebApp.directive("loadingIndicator", function() {
+	return {
+		restrict : "A",
+		template : "<div>Loading...</div>",
+		link : function(scope, element, attrs) {
+			scope.$on("loading-started", function(e) {
+				element.css({
+					"display" : ""
+				});
+			});
+
+			scope.$on("loading-complete", function(e) {
+				element.css({
+					"display" : "none"
+				});
+			});
+
+		}
+	};
+});
+
+
+
+mythTVWebApp.controller('RecordingsController', [
+		'$scope',
+		'$http',
+		function($scope, $http) {
+			$scope.loadData = function() {
+				$http.get('http://'+BACKEND+':'+PORT+RECORDINGS_PATH).success(
+						function(data) {
+							$scope.recordings = data.ProgramList.Programs;
+						});
+			}
+			$scope.loadData();
+			$scope.recordingsPredicate = '-AirDate';
+		} ]);
